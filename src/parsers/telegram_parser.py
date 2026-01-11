558f61454c081feb5f6d14454ab5d2bbe0cb9ee9
Sergey <SVTyukin@yandex.ru>
2026-01-11 12:47:44 +0300

    refactor: пераработан модуль парсинга - Все лишнее убрано из асинхронной функции в основную функцию модуля - Запуск логирования и подготовка путей убрано из корня в основную функцию модуля

--- src/parsers/telegram_parser.py
+++ src/parsers/telegram_parser.py
@@ -4,44 +4,19 @@ import json
 import yaml
 import logging
 
+
 from src.utils.config_loader import setup_logging, load_secrets, PROJECT_ROOT
 
 
-INPUT_FILE = PROJECT_ROOT / "config" / "telegram_channels.yaml"
-OUTPUT_FILE = PROJECT_ROOT / "data" / "raw" / "news.json"
-SESSION_FILE = PROJECT_ROOT / "data" / "sessions" / "telegram_parser"
-
-setup_logging('parser.log')
-logger = logging.getLogger(__name__)
-
-secrets = load_secrets()
-
-async def main():
-    client = TelegramClient(SESSION_FILE, secrets["telegram"]["api_id"], secrets["telegram"]["api_hash"])
+async def fetch_telegram_news(secrets, session_file, channels, last_ids, logger):
+    client = TelegramClient(session_file, secrets["telegram"]["api_id"], secrets["telegram"]["api_hash"])
 
     await client.start(phone=secrets["telegram"]["phone"])
     logger.info("Клиент для парсинга новостей из Telegram запущен!")
 
-    with open(INPUT_FILE, 'r', encoding='utf-8') as f:
-        CHANNELS = [url.strip() for url in yaml.safe_load(f) if isinstance(url, str)]
-
-    # Загружаем существующие новости, чтобы определить последний ID по каждому каналу
-    existing_news = []
-    if OUTPUT_FILE.exists():
-        with open(OUTPUT_FILE, 'r', encoding='utf-8') as f:
-            existing_news = json.load(f)
-
-    # Собираем последние ID
-    last_ids = {}
-    for item in existing_news:
-        channel = item['channel']
-        msg_id = item['message_id']
-        if channel not in last_ids or msg_id > last_ids[channel]:
-            last_ids[channel] = msg_id
-
     news = []
 
-    for channel in CHANNELS:
+    for channel in channels:
         try:
             entity = await client.get_entity(channel)
             messages = await client.get_messages(entity, limit=50)
@@ -54,7 +29,7 @@ async def main():
             for msg in messages:
                 if msg.id <= last_id:
                     break
-                if msg.text.strip():
+                if msg.text is not None and msg.text.strip():
                     news_item = {
                         'channel': entity.title,
                         'message_id': msg.id,
@@ -69,18 +44,54 @@ async def main():
         except Exception as e:
             logger.error(f"Ошибка при парсинге {channel}: {e}")
 
-    all_news = existing_news + news
-
-    with open(OUTPUT_FILE, 'w', encoding='utf-8') as f:
-        json.dump(all_news, f, ensure_ascii=False, indent=2)
-
-    if len(news) > 0:
-        logger.info(f"Сохранено {len(news)} новостей в {OUTPUT_FILE} из канала {channel}")
-
     await client.disconnect()
+    return news
+
 
 def run_telegram_parser():
-    asyncio.run(main())
+    setup_logging('parser.log')
+    logger = logging.getLogger(__name__)
+
+    input_file = PROJECT_ROOT / "config" / "telegram_channels.yaml"
+    output_file = PROJECT_ROOT / "data" / "raw" / "news.json"
+    session_file = PROJECT_ROOT / "data" / "sessions" / "telegram_parser"
+    secrets = load_secrets()
+
+    with open(input_file, 'r', encoding='utf-8') as f:
+        channels = [url.strip() for url in yaml.safe_load(f) if isinstance(url, str)]
+
+    # Загружаем существующие новости, чтобы определить последний ID по каждому каналу
+    existing_news = []
+    if output_file.exists():
+        with open(output_file, 'r', encoding='utf-8') as f:
+            existing_news = json.load(f)
+
+    # Собираем последние ID
+    last_ids = {}
+    for item in existing_news:
+        channel = item['channel']
+        msg_id = item['message_id']
+        if channel not in last_ids or msg_id > last_ids[channel]:
+            last_ids[channel] = msg_id
+
+    news = asyncio.run(fetch_telegram_news(
+        secrets=secrets,
+        session_file=session_file,
+        channels=channels,
+        last_ids=last_ids,
+        logger=logger
+    ))
+
+    if len(news) > 0:
+        all_news = existing_news + news
+
+        with open(output_file, 'w', encoding='utf-8') as f:
+            json.dump(all_news, f, ensure_ascii=False, indent=2)
+
+        logger.info(f"Сохранено {len(news)} новостей из {len(channels)} в {output_file}")
+    else:
+        logger.info(f"Файл {output_file} оставлен без изменений")
+
 
 if __name__ == '__main__':
     run_telegram_parser()
