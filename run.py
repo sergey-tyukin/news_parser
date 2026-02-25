fa083e282e7e676d4e251c033e4bcc1c7d3dd403
Sergey <SVTyukin@yandex.ru>
2026-02-25 07:07:24 +0300

    refactor: использование функции с обработкой ошибок для SQL-запросов

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 from src.parsers import telegram_parser
-from src.news_processing import remove_links, extract_companies
+from src.news_processing import cleaners, extract_companies
 from src.sentiment import sentiment
 
+# Парсим свежие новости
 telegram_parser.run_telegram_parser()
-remove_links.remove_links()
-extract_companies.extract_companies()
-sentiment.get_sentiment()
+
+# Подготоваливаем новости
+cleaners.run_news_cleaner()
+
+
+# extract_companies.extract_companies()
+# sentiment.get_sentiment()
