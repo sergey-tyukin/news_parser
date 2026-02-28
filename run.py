258974ff8778247e432bc4239549eaeb9058f837
Sergey <SVTyukin@yandex.ru>
2026-02-28 08:19:27 +0300

    feat: реализован подсчёт индекса сентимента с записью в БД

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from src.utils.config_loader import DB_PATH
 from src.utils import create_db, insert_channels_into_db
 from src.parsers import telegram_parser
 from src.news_processing import cleaners, extract_companies
-from src.sentiment import sentiment
+from src.sentiment import calc_news_sentiment, calc_sentiment_index
 
 # Создаём базу, если её нет
 if not Path(DB_PATH).is_file():
@@ -19,5 +19,8 @@ cleaners.run_news_cleaner()
 # Ищем названия компаний в новостях
 extract_companies.extract_companies()
 
-# Определяем сентимент
-sentiment.get_sentiment()
+# Определяем сентимент новостей
+calc_news_sentiment.get_sentiment()
+
+# Считаем индекс сентимента
+calc_sentiment_index.calculate_index()
